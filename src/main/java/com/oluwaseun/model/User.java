2f8faebdba6b959e239b24853a39875e3f0fe753
Seun <olagunjuoluwaseun1@gmail.com>
2022-03-14 10:43:34 +0100

    User Login App With SpringBoot

---
+++ src/main/java/com/oluwaseun/model/User.java
@@ -0,0 +1,50 @@
+package com.oluwaseun.model;
+
+import java.sql.Date;
+
+public class User {
+	
+	private String firstName;
+	private String lastName;
+	private String email;
+	private String password;
+	private Date dob;
+	
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public Date getDob() {
+		return dob;
+	}
+	public void setDob(Date dob) {
+		this.dob = dob;
+	}
+	@Override
+	public String toString() {
+		return "User [firstName=" + firstName + ", lastName=" + lastName + ", email=" + email + ", password=" + password
+				+ ", dob=" + dob + "]";
+	}
+
+}
